1ee1dc29754b19a99fe9532f3298749caa5789af
vu van dao <vvdao096@gmail.com>
2023-06-28 00:03:58 +0700

    test

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
+var os = require("os");
+var hostname = os.hostname();
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' + hostname;
   }
 }
