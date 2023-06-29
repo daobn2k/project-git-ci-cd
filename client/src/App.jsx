ddb9f7bf703a13c360b0f9703f121581eb5dd24b
vu van dao <vvdao096@gmail.com>
2023-06-29 21:41:53 +0700

    test vớ vẩn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(0);
+
+  const urlApi = import.meta.env.VITE_API_URL;
+
+  console.log(urlApi, 'urlApi');
+  const testGet = async () => {
+    const res = await axios.get(urlApi ? urlApi : 'http://localhost:8080/');
+
+    setData(res?.data);
+  };
+  useEffect(() => {
+    testGet();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>test truy cập private : {data}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
